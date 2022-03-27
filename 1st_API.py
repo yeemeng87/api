3c3714aa27e3a0d40fb598b43f39bbaa21a411ca
yeemeng87 <95422526+yeemeng87@users.noreply.github.com>
2022-03-27 17:05:45 +0800

    Initial version

---
+++ 1st_API.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 26 09:55:05 2022
+
+@author: Yee Meng
+"""
+
+# 1st python API
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+attractions = {'Names': ['Marina Bay Sands', 'Gardens by the Bay', 'Botanic Gardens']}
+
+@app.route("/attractions", methods=["GET"])
+def get_attractions():
+    return jsonify(attractions)
